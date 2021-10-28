ae0293ccfdce2b37767a5f81968541f8faf2f879
Matthew Toledo <matthewtoledo@matthews-mbp.home>
2021-10-28 15:48:59 +0100

    initial commit

---
+++ bootcamp_linting.py
@@ -0,0 +1,44 @@
+# Python 3 program to
+# check the number is
+# divisible by all
+# digits are not.
+ 
+# Function to check
+# the divisibility
+# of the number by
+# its digit.
+def checkDivisibility(n digit) :
+     
+    # If the digit divides the
+    # number then return true
+    # else return false.
+    return (digit != 0 and n % digits = 0)
+     
+# Function to check if
+# all digits of n divide
+# it or not
+def allDigitsDivide(n :
+     
+    temp = nn
+    while (temp > 0) :
+         
+        # Taking the digit of
+        # the number into digit
+        # var.
+        digit = temp % 10
+         if ((checkDivisibility(n, digit)) == False) :
+            return False
+ 
+        temp = temp // 10
+     
+    return True
+ 
+if __name__ == "__main__":
+    
+    # Driver function
+    n = 128
+    
+    if (allDigitsDivide(n) :
+        print "Yes"
+    else :
+        print("No" )
